4c0700f0dcce3a9a7d78de09a5d6a330e58fefae
irene lope rodriguez <ilope@iota10.aulas.gsyc.es>
2014-11-17 15:58:49 +0100

    Modificados los errores

--- server.py
+++ server.py
@@ -5,27 +5,44 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
 
+
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    CANCION = sys.argv[3]
+except IndexError:
+    print 'Usage: python server.py IP port audio_file'
+    raise SystemExit
+except ValueError:
+    print 'Usage: python server.py IP port audio_file'
+    raise SystemExit
+
+metodos = ('INVITE', 'BYE', 'ACK')
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP Server
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        print 'Listening...'
+        self.wfile.write("Hemos recibido tu peticion\r\n")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
-                break
+                break            
+            print "El cliente nos manda " + line
 
+            #Obtenemos el método del cliente
+            metodo = line.split()[0]
+            
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
