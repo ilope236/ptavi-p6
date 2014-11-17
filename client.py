a010a33243b2aa0cbd2a7e944c82420d2da4149c
irene lope rodriguez <ilope@iota04.aulas.gsyc.es>
2014-11-17 10:51:32 +0100

    Modificados los errores y la linea de comandos

--- client.py
+++ client.py
@@ -5,12 +5,28 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+#Comprobamos errores en los datos
+metodos = ('INVITE', 'BYE')
+
+try:
+    METODO = sys.argv[1]
+    USER = sys.argv[2].split('@')[0]
+    IP = sys.argv[2].split('@')[1].split(':')[0]
+    PORT = int(sys.argv[2].split(':')[1])
+    if not METODO in metodos:
+        print 'Usage: python client.py method receiver@IP:SIPport'
+        raise SystemExit
+except IndexError:
+    print 'Usage: python client.py method receiver@IP:SIPport'
+    raise SystemExit
+except ValueError:
+    print 'Usage: python client.py method receiver@IP:SIPport'
+    raise SystemExit
+
 
 # Contenido que vamos a enviar
 LINE = '¡Hola mundo!'
@@ -18,11 +34,17 @@ LINE = '¡Hola mundo!'
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
 
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+
+#Comprobamos que se escucha en el servidor
+try:
+    data = my_socket.recv(1024)
+except socket.error:
+    print 'Error: No server listening at ' + IP + ' port ' + str(PORT)
+    raise SystemExit
 
 print 'Recibido -- ', data
 print "Terminando socket..."
